5d72d2e5bb7bc8dd4a431e623936bb8a2ef49381
Facundo Rivas <facundo.rivas@ing.austral.edu.ar>
2021-04-08 22:09:48 -0300

    Google java format added

--- lexer/src/main/java/logic/LexemeMatcher.java
+++ lexer/src/main/java/logic/LexemeMatcher.java
@@ -5,10 +5,4 @@ import token.TokenType;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public interface LexemeMatcher {
-
-    Pattern getPattern();
-    TokenType getTokenType();
-    Matcher getMatcher(String input);
-
-}
+public interface LexemeMatcher { Pattern getPattern();TokenType getTokenType();Matcher getMatcher(String input);}
